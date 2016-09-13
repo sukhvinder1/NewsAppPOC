c74057a7f668e61f7d2d85c5da4bd60d881df84d
Sukhvinder <sukhvindersingh02@gmail.com>
2016-09-13 11:48:32 -0400

    Update MainActivity.java

--- app/src/main/java/com/learning/sukhu/newsapppoc/MainActivity.java
+++ app/src/main/java/com/learning/sukhu/newsapppoc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.learning.sukhu.newsapppoc;
 
+import android.app.ListActivity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
@@ -7,6 +8,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import com.learning.sukhu.newsapppoc.Json.GetJsonData;
@@ -18,32 +22,30 @@ public class MainActivity extends AppCompatActivity implements DataBus{
     private GetJsonData jsonData;
     private List<Sources> sourcesData;
     private String LOG_TAG = "Sukh_Tag_MainActivity";
+    private ListView listView;
+    private String[] sourcesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.v(LOG_TAG, "onCreate");
+        listView = (ListView) findViewById(R.id.listView);
+        sourcesData = new ArrayList<Sources>();
+        if(isNetworkAvailable()){
+            jsonData = new GetJsonData("Hello", this);
+            jsonData.execute();
+        }else {
+            Toast.makeText(this,"Sorry!! I don't have Internet Power Right now !", Toast.LENGTH_LONG).show();
+            listView.setVisibility(View.INVISIBLE);
+        }
         /*GetRawJsonData theRawData = new GetRawJsonData("https://newsapi.org/v1/sources/");
         theRawData.execute();*/
-
     }
 
     public void onStart(){
         super.onStart();
-        if(isNetworkAvailable()){
-            jsonData = new GetJsonData("Hello", this);
-            jsonData.execute();
-            sourcesData = new ArrayList<Sources>();
-        }else {
-            Toast.makeText(this,"Sorry!! I don't have Internet Power Right now !", Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void onTouch(View v){
-        Toast.makeText(this,"Hello", Toast.LENGTH_LONG).show();
-        for(Sources source : sourcesData){
-            Log.v(LOG_TAG, source.toString());
-        }
+        Log.v(LOG_TAG, "onStart");
     }
 
     private boolean isNetworkAvailable() {
@@ -55,12 +57,29 @@ public class MainActivity extends AppCompatActivity implements DataBus{
 
     public void onPause(){
         super.onPause();
-        jsonData = null;
+        //jsonData = null;
+        Log.v(LOG_TAG, "onPause");
     }
 
     @Override
-    public void processData(List<Sources> sources) {
+    public void processedData(List<Sources> sources) {
         Log.v(LOG_TAG, "transferring Data");
         this.sourcesData = sources;
+        sourcesList = new String[sourcesData.size()];
+        int i=0;
+        for(Sources source : sourcesData){
+            Log.v(LOG_TAG, source.getName());
+            sourcesList[i] = source.getName();
+            i++;
+        }
+        ArrayAdapter<String> myArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sourcesList);
+        listView.setAdapter(myArrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(view.getContext(), sourcesList[position], Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
