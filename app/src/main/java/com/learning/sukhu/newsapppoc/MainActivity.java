ae19c32b790d9f3fbaaa78311402a2ad3b3862ae
Sukhvinder <sukhvindersingh02@gmail.com>
2016-09-18 11:47:11 -0400

    update-unstable

--- app/src/main/java/com/learning/sukhu/newsapppoc/MainActivity.java
+++ app/src/main/java/com/learning/sukhu/newsapppoc/MainActivity.java
@@ -1,15 +1,18 @@
 package com.learning.sukhu.newsapppoc;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.view.Window;
-import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.learning.sukhu.newsapppoc.Json.GetJsonData;
@@ -21,20 +24,48 @@ public class MainActivity extends AppCompatActivity implements DataBus{
     private GetJsonData jsonData;
     private String LOG_TAG = "Sukh_Tag_MainActivity";
     private ListView listView;
+    private GridView gridView;
+    private ImageView imageView;
+    private TextView textView;
     private ArrayList<Sources> sourceList;
+    private int USER_PREFRENCE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         Log.v(LOG_TAG, "onCreate");
+        setContentView(R.layout.activity_main);
+    }
+
+    private void getUserPref(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        USER_PREFRENCE = sharedPreferences.getInt("Key", 0);
+        Log.v(LOG_TAG, "PREF - " + USER_PREFRENCE);
+        if(USER_PREFRENCE==0){
+            Log.v(LOG_TAG, "FIRST TIME");
+            updatePref(sharedPreferences);
+        }else{
+            Log.v(LOG_TAG, "NOT FIRST TIME");
+            updatePref(sharedPreferences);
+        }
+    }
+
+    private void updatePref(SharedPreferences sharedPreferences){
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        USER_PREFRENCE++;
+        editor.putInt("Key", USER_PREFRENCE);
+        editor.commit();
     }
 
     protected void onStart(){
         super.onStart();
         //initializing all the variables
-        listView = (ListView) findViewById(R.id.listView);
         sourceList = new ArrayList<>();
+        listView = (ListView) findViewById(R.id.listView);
+        gridView = (GridView) findViewById(R.id.gridView);
+        imageView = (ImageView) findViewById(R.id.imageView);
+        textView = (TextView) findViewById(R.id.sourcesList);
+        getUserPref();
 
         //checking if network is available
         if(isNetworkAvailable()){
@@ -64,16 +95,27 @@ public class MainActivity extends AppCompatActivity implements DataBus{
         //creating sourceList of Sources
         sourceList.addAll(sources);
         //custom adapter
+        if (USER_PREFRENCE % 2 == 0) {
+            // even
+            Log.v(LOG_TAG, "EVEN");
+            listView.setVisibility(View.INVISIBLE);
+            gridView.setVisibility(View.VISIBLE);
+        } else {
+            // odd
+            Log.v(LOG_TAG, "ODD");
+            listView.setVisibility(View.VISIBLE);
+            gridView.setVisibility(View.INVISIBLE);
+        }
         CustomListAdapter adapter = new CustomListAdapter(
-                getApplicationContext(), R.layout.custom_list_layout, sourceList);
-        listView.setAdapter(adapter);
+                getApplicationContext(), R.layout.custome_grid_layout, sourceList);
+        gridView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        /*listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Toast.makeText(view.getContext(), sourceList.get(position).getName(), Toast.LENGTH_LONG).show();
             }
-        });
+        });*/
     }
 
     protected void onStop(){
